f7bcada38ae1ba303459d0aa0b3e2052a5a70e0b
jesper53 <74552164+jesper-stack@users.noreply.github.com>
2021-06-16 11:56:23 +0000

    computer kiest woord uit lijst

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+# name = input("Wat is je naam? ")
+# print("Welkom " + name)
+Woorden = ["Boom", "Roos", "Vis"]
+for woord in Woorden :
+  print(woord)
+print(random.choice(Woorden))
