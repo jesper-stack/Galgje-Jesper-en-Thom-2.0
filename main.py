577ad914a1914ee033642ef047ec18b8de0a0124
jesper53 <74552164+jesper-stack@users.noreply.github.com>
2021-06-21 13:00:20 +0000

    letter in woord laten zien en onthouden

--- main.py
+++ main.py
@@ -4,15 +4,29 @@ print("Welkom " + name)
 Woorden = ["Boom", "Roos", "Vis"]
 ##
 gekozenWoord = random.choice(Woorden)
-
+goedgekozenletters = []
+foutegekozenletters = []
 def galgje():
+  print("Dit zijn de letters die je fout hebt gekozen")
+  print(foutegekozenletters)
+  print("je hebt de volgende letter gekozen")
+  print(goedgekozenletters)
   print("\nKies een letter")
   gekozenletter = input()
+
+  if gekozenletter in gekozenWoord :       
+    goedgekozenletters.append(gekozenletter)
+  else :           
+    foutegekozenletters.append(gekozenletter)
+
+
   for letter in gekozenWoord :
-    if gekozenletter == letter : 
-      print(gekozenletter, end= "")
+    if letter in goedgekozenletters : 
+      print(letter, end= "")
     else :     
-      print("_ ", end = "")
+      print("_ ", end = "")      
+
+
  
   
   galgje()
