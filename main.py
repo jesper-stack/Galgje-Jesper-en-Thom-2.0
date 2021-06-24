452d171e7a5d594ed29230c7355e53fbac371c1b
jesper53 <74552164+jesper-stack@users.noreply.github.com>
2021-06-24 13:49:47 +0000

    Netjes gemaakt

--- main.py
+++ main.py
@@ -4,33 +4,33 @@ print("Welkom " + name)
 Woorden = ["boom", "roos", "vis"]
 print("je begint met 6 beurten")
 ##
-gekozenWoord = random.choice(Woorden)
-goedgekozenletters = []
-foutegekozenletters = []
-aantal_beurten = 6 
-def galgje(beurten_over):
-  print("\nDit zijn de letters die je fout hebt gekozen")
-  print(foutegekozenletters)
-  print("Dit zijn de letters die je goed hebt gekozen")
-  print(goedgekozenletters)
-  print("\nKies een letter")
-  gekozenletter = input()
-  
-  if gekozenletter in gekozenWoord :       
-    goedgekozenletters.append(gekozenletter)
-  else : 
-    foutegekozenletters.append(gekozenletter)
-    beurten_over -=1 
-    print("Je hebt nog", aantal_beurten -1, "beurten over")
 
-  for letter in gekozenWoord :
-    if letter in goedgekozenletters : 
-      print(letter, end= "")
-    else :     
-      print("_ ", end = "")  
-  return beurten_over       
+def galgje():
+  gekozenWoord = random.choice(Woorden)
+  goedgekozenletters = []
+  foutegekozenletters = []
+  aantal_beurten = 6 
+  while aantal_beurten > 0 :
+    print("\nDit zijn de letters die je fout hebt gekozen")
+    print(foutegekozenletters)
+    print("Dit zijn de letters die je goed hebt gekozen")
+    print(goedgekozenletters)
+    gekozenletter = input("\nKies een letter: ")
+    
+    if gekozenletter in gekozenWoord :       
+      goedgekozenletters.append(gekozenletter)
+    else : 
+      foutegekozenletters.append(gekozenletter)
+      aantal_beurten -=1 
+      print("Je hebt nog", aantal_beurten , "beurten over")
+
+    for letter in gekozenWoord :
+      if letter in goedgekozenletters : 
+        print(letter, end= "")
+      else :     
+        print("_ ", end = "")  
+       
+galgje()
 
 
-while aantal_beurten > 0 :
-  aantal_beurten= galgje(aantal_beurten) 
 
