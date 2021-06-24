4c56680b352ceecd30edc5517779347fa0379822
jesper53 <74552164+jesper-stack@users.noreply.github.com>
2021-06-24 13:41:30 +0000

    Klaar met beurtencounter

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ print("je begint met 6 beurten")
 gekozenWoord = random.choice(Woorden)
 goedgekozenletters = []
 foutegekozenletters = []
-aantal_beurten = 6
-def galgje():
+aantal_beurten = 6 
+def galgje(beurten_over):
   print("\nDit zijn de letters die je fout hebt gekozen")
   print(foutegekozenletters)
-  print("je hebt de volgende goed letter gekozen")
+  print("Dit zijn de letters die je goed hebt gekozen")
   print(goedgekozenletters)
   print("\nKies een letter")
   gekozenletter = input()
   
   if gekozenletter in gekozenWoord :       
     goedgekozenletters.append(gekozenletter)
-  else :           
+  else : 
     foutegekozenletters.append(gekozenletter)
+    beurten_over -=1 
     print("Je hebt nog", aantal_beurten -1, "beurten over")
 
   for letter in gekozenWoord :
@@ -27,9 +28,9 @@ def galgje():
       print(letter, end= "")
     else :     
       print("_ ", end = "")  
+  return beurten_over       
 
 
+while aantal_beurten > 0 :
+  aantal_beurten= galgje(aantal_beurten) 
 
-galgje()
-
-galgje()
